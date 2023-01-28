91caae15f5daab3f46a6e5b1db7c723c06580080
ramibelhadjsalem <60022426+ramibelhadjsalem@users.noreply.github.com>
2023-01-28 21:35:25 +0100

    sidebar reparation page

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +10,12 @@ export class HeaderComponent implements OnInit {
 
   isMenuCollapsed = true;
 
-  constructor() { }
+  constructor(private route : Router) {
+    route.events.subscribe(()=> this.isMenuCollapsed =true)
+   }
 
   ngOnInit(): void {
+    
   }
 
 }
